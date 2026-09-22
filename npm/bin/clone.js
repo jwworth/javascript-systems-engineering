812ab93b6c433502e534534f198bf59cd7a6c9bc
Jake Worth <jake@jakeworth.com>
2026-09-22 11:03:24 -0400

    Add clone command

---
+++ npm/bin/clone.js
@@ -0,0 +1,11 @@
+const {execSync} = require('child_process');
+
+const repoURL = 'git@github.com:lodash/lodash.git';
+
+try {
+  console.log('Cloning lodash...');
+  execSync(`git clone ${repoURL}`, {stdio: 'inherit'});
+  console.log('Cloned!');
+} catch (error) {
+  console.log('Failed to clone repo: ', error);
+}
